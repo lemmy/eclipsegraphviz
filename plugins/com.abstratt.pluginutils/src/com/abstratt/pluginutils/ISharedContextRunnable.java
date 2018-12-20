55fd934a31749c94a729c42116f372af72b4743f
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2018-12-20 10:03:15 -0800

    Host p2 repository as gh-page

--- plugins/com.abstratt.pluginutils/src/com/abstratt/pluginutils/ISharedContextRunnable.java
+++
@@ -1,8 +0,0 @@
-package com.abstratt.pluginutils;
-
-/**
- * A runnable that does not need to make changes.
- */
-public interface ISharedContextRunnable<C, R> {
-    public R runInContext(C context);
-}
--- plugins/com.abstratt.pluginutils/src/com/abstratt/pluginutils/UserFacingException.java
+++
@@ -1,5 +0,0 @@
-package com.abstratt.pluginutils;
-
-public interface UserFacingException {
-    public String getUserFacingMessage();
-}
